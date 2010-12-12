f6b5866dab70c28dff410a7b67ae283d71a5bb7a
bobz <bobz@zeus.bobz.in>
2010-12-12 01:58:14 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goaldigr::Application.config.secret_token = '54a2b9501434752f3b85b1b3cf05fc37c1f77cd6105900b2530ff8875c6098e90608f93bff3a7a2b552cfc1941341a6414de837d9124ff166f43ce61f7fe7435'
