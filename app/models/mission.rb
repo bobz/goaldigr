73909cf41a3150936e6ead6fdbfb12492fd50c55
bobz <bobzin44@gmail.com>
2010-12-19 23:05:15 +0000

    add support for day/week/month totals

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,4 +1,21 @@
 class Mission < ActiveRecord::Base
   has_many :acts, :dependent => :destroy
   belongs_to :goal
+
+  def acts_day
+	yesterday = Time.zone.now - 1.day
+	logger.info yesterday.to_s(:rfc822)
+    acts.where("created_at >= ?", yesterday).count
+  end
+  def acts_week
+	yesterday = Time.zone.now - 7.day
+	logger.info yesterday.to_s(:rfc822)
+    acts.where("created_at >= ?", yesterday).count
+  end
+  def acts_month
+	yesterday = Time.zone.now - 30.day
+	logger.info yesterday.to_s(:rfc822)
+    acts.where("created_at >= ?", yesterday).count
+  end
+
 end
