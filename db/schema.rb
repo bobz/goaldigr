82436c389418ba962046e11e66481cc1355fe854
bobz <bobzin44@gmail.com>
2011-01-02 02:26:37 +0000

    basic clearance install

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101219231712) do
+ActiveRecord::Schema.define(:version => 20110102021736) do
 
   create_table "acts", :force => true do |t|
     t.integer  "mission_id"
@@ -33,5 +33,19 @@ ActiveRecord::Schema.define(:version => 20101219231712) do
     t.string   "title"
     t.text     "description"
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "encrypted_password", :limit => 128
+    t.string   "salt",               :limit => 128
+    t.string   "confirmation_token", :limit => 128
+    t.string   "remember_token",     :limit => 128
+    t.boolean  "email_confirmed",                   :default => false, :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email"
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
