417794b15335b8de748de5351c244e504afae86d
bobz <bobzin44@gmail.com>
2011-03-05 21:21:04 +0000

    420 blunt break

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,17 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function missionActAdd(url)
+{
+  new Ajax.Request(url,{
+    onSuccess: function(response){
+      alert("Created Act");
+	},
+	onFailure: function(response){
+	  alert("Failed to create act");
+	},
+	onComplete: function(response){
+	  alert("Complete")
+	}
+  });
+}
