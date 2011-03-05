b6fb903403a232a549ab4bff36ba849906bb1773
bobz <bobzin44@gmail.com>
2011-03-05 21:50:33 +0000

    my first ajax FUCK YEA

--- app/controllers/acts_controller.rb
+++ app/controllers/acts_controller.rb
@@ -2,6 +2,10 @@ class ActsController < ApplicationController
   def create
     mission = Mission.find(params[:mission_id])
     @act = Act.create(:mission => mission)
-    redirect_to(missions_path, :notice => 'Act was successfully created.')
+	
+	render :json => [mission.acts_day, mission.acts_week, 
+	  mission.acts_month, mission.acts.count]
+	
+	
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,17 +1,19 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-function missionActAdd(url)
+function missionActAdd(url, mission_id)
 {
   new Ajax.Request(url,{
     onSuccess: function(response){
-      alert("Created Act");
+      var json = response.responseJSON;
+	  console.log("JSON", json);
+
+	  var row = $('mission_'+ mission_id);
+      row.down('.day').update(json[0]);
+
 	},
 	onFailure: function(response){
 	  alert("Failed to create act");
-	},
-	onComplete: function(response){
-	  alert("Complete")
 	}
   });
 }
